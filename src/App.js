0f5ac9d1fa35c811bb59a3e4229c11880e8133cc
Yarik <therakong@gmail.com>
2020-01-13 17:11:29 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import images from './img'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todos = [{todo:"create app",done:false}, {todo:'go to gym',done:false}, {todo:'buy some food',done:false}];
+const colors = ['danger', 'primary', 'secondary', 'success', 'warning', 'info', 'dark'];
+var uniqid = require('uniqid');
+
+class App extends React.Component {
+  state = {
+    value: "",
+    background: 20,
+    done: true,
+  }
+  handleChange = (e) => {
+    this.setState({value: e.target.value});
+    if(this.state.background < 42){
+      this.setState({background: this.state.background + 1})
+    }else{
+      this.setState({background: 1})
+    }
+    
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    todos.push({todo:this.state.value,done: false});
+    this.setState({value: ""})
+  }
+  done = (e) => {
+    if(todos[e].done){
+      todos.map((todo, index) => index != e ? null:  todo.done == true ? todos.splice(e,1): null);
+    }else{
+      todos.map((todo, index) => index === e ? todo.done = true:null );
+    }
+    this.setState({done: !this.state.done});
+  }
+
+    render() {
+        return (
+          <div className = "app" style = {{background: `url(${images[this.state.background]})`, backgroundSize: 'cover'}}> 
+          {/* "url(`./chan/${this.state.background}`)" */}
+            <div className = 'content'>
+              <h1 className="text-center">Yarik to-do</h1>
+              <form onSubmit={this.handleSubmit}>
+                <div className="row mb-5">
+                    <div className="col-10">
+                      <input value = {this.state.value} onChange = {this.handleChange} className="form-control" placeholder = "tap your todo's"></input>
+                    </div>
+                    <div className="col-2">
+                      <button type="submit" className="btn btn-danger">enter</button>
+                    </div>
+                </div>
+              </form>
+              {todos.map((todo, index)=> <li onClick = { () => this.done(index)} className={`list-group-item list-group-item-${colors[Math.floor(Math.random() * Math.floor(6))]} ${todo.done ? "done" : ""}`} key = {uniqid()}>{todo.todo}</li>) }
+              {/* {todos.map((todo, index) => console.log(todo.todo))}
+              {todos.map((todo,index) => console.log(`list-group-item list-group-item-${colors[index]}`))} */}
+            </div>
+        </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/img.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react'
+import img1 from './chan/1.jpg';
+import img2 from './chan/2.jpg';
+import img3 from './chan/3.jpg';
+import img4 from './chan/4.jpg';
+import img5 from './chan/5.jpg';
+import img6 from './chan/6.jpg';
+import img7 from './chan/7.jpg';
+import img8 from './chan/8.jpg';
+import img9 from './chan/9.jpg';
+import img10 from './chan/10.jpg';
+import img11 from './chan/11.jpg';
+import img12 from './chan/12.jpg';
+import img13 from './chan/13.jpg';
+import img14 from './chan/14.jpg';
+import img15 from './chan/15.jpg';
+import img16 from './chan/16.jpg';
+import img17 from './chan/17.jpg';
+import img18 from './chan/18.jpg';
+import img19 from './chan/19.jpg';
+import img20 from './chan/20.jpg';
+import img21 from './chan/21.jpg';
+import img22 from './chan/22.jpg';
+import img23 from './chan/23.jpg';
+import img24 from './chan/24.jpg';
+import img25 from './chan/25.jpg';
+import img26 from './chan/26.jpg';
+import img27 from './chan/27.jpg';
+import img28 from './chan/28.jpg';
+import img29 from './chan/29.jpg';
+import img30 from './chan/30.jpg';
+import img31 from './chan/31.jpg';
+import img32 from './chan/32.jpg';
+import img33 from './chan/32.jpg';
+import img34 from './chan/33.jpg';
+import img35 from './chan/34.jpg';
+import img36 from './chan/35.jpg';
+import img37 from './chan/36.jpg';
+import img38 from './chan/37.jpg';
+import img39 from './chan/38.jpg';
+import img40 from './chan/39.jpg';
+import img41 from './chan/40.jpg';
+import img42 from './chan/41.jpg';
+import img43 from './chan/42.jpg';
+
+
+const images = [
+    img1,
+    img2,
+    img3 ,
+    img4 ,
+    img5,
+    img6 ,
+    img7 ,
+    img8 ,
+    img9 ,
+    img10 ,
+    img11 ,
+    img12 ,
+    img13 ,
+    img14 ,
+    img15 ,
+    img16 ,
+    img17 ,
+    img18 ,
+    img19 ,
+    img20 ,
+    img21 ,
+    img22 ,
+    img23 ,
+    img24 ,
+    img25 ,
+    img26 ,
+    img27 ,
+    img28 ,
+    img29 ,
+    img30 ,
+    img31 ,
+    img32 ,
+    img33 ,
+    img34 ,
+    img35 ,
+    img36,
+    img37 ,
+    img38 ,
+    img39 ,
+    img40 ,
+    img41 ,
+    img42,
+    img43 ,
+]
+
+export default images;
